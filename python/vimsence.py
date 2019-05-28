0e6c5bbdd76ffd63a7a1485ee1e99809adc3b6c5
AnanaGame <gameanana@gmail.com>
2019-05-28 19:09:51 +0200

    Add backslash support, #1

--- python/vimsence.py
+++ python/vimsence.py
@@ -83,4 +83,4 @@ def get_directory():
     """Get current directory
     :returns: string
     """
-    return vim.eval('getcwd()').split('/')[-1]
+    return vim.eval('getcwd()').split('(\/)|(\\)')[-1]
