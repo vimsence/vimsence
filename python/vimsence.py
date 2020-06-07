19b80fe0c2f1b6de12671222ab06c146b8a1b6b2
Hugo Lageneste <hugo@olivia-ai.org>
2020-06-07 17:18:20 +0200

    Change elixir

--- python/vimsence.py
+++ python/vimsence.py
@@ -35,7 +35,7 @@ if (vim.eval("exists('{}')".format("g:vimsence_app_id")) == "1"):
 has_thumbnail = [
     'c', 'cr', 'hs', 'json', 'nim', 'ruby', 'cpp', 'go', 'javascript', 'markdown',
     'typescript', 'python', 'vim', 'rust', 'css', 'html', 'vue', 'paco', 'tex', 'sh',
-    'elixir', 'cs'
+    'ex', 'cs'
 ]
 
 # Remaps file types to specific icons.
@@ -45,8 +45,7 @@ remap = {
         "ruby": "rb", 
         "rust": "rs", 
         "typescript": "ts",
-        "javascript": "js",
-        "elixir": "ex"
+        "javascript": "js"
 }
 
 file_explorers = [
