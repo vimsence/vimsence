b64ce5a5f7f31346e29f06ec72cff321c438acb7
Hugo Lageneste <hugo@olivia-ai.org>
2021-02-20 20:31:33 +0100

    Update python/vimsence.py
    Co-authored-by: Anne Douwe Bouma <annedouwe@bouma.tech>

--- python/vimsence.py
+++ python/vimsence.py
@@ -36,7 +36,7 @@ if (vim.eval("exists('{}')".format("g:vimsence_client_id")) == "1"):
 has_thumbnail = {
     'c', 'cr', 'hs', 'json', 'nim', 'ruby', 'cpp', 'go', 'javascript', 'markdown',
     'typescript', 'python', 'vim', 'rust', 'css', 'html', 'vue', 'paco', 'tex', 'sh',
-    'elixir', 'cs', 'ocaml
+    'elixir', 'cs', 'ocaml'
 }
 
 # Remaps file types to specific icons.
